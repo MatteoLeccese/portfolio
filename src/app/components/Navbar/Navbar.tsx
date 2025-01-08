4addea408a86248f29457a5f28a46622c6663c57
Matteo Leccese <matteoleccese2099@gmail.com>
2025-01-08 18:25:52 -0400

    making the base frontpage, and modifying the header

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -3,43 +3,46 @@ import "./index.css";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <Image
-        className="nav-logo"
-        src="/ml_logo_no_background.png"
-        width={64}
-        height={64}
-        alt="logo"
-        quality={50}
-        loading="lazy"
-      />
-      <ul>
-        <li>
-          <a href="#Home">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#Work">
-            Work
-          </a>
-        </li>
-        <li>
-          <a href="#Projects">
-            Projects
-          </a>
-        </li>
-        <li>
-          <a href="#About">
-            About
-          </a>
-        </li>
-        <li>
-          <button className="pdf-button">
-            Download PDF
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <Image
+          className="nav-logo"
+          src="/ml_logo_no_background.png"
+          width={64}
+          height={64}
+          alt="logo"
+          quality={50}
+          loading="lazy"
+          priority={false}
+        />
+        <ul>
+          <li>
+            <a href="#Home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#Work">
+              Work
+            </a>
+          </li>
+          <li>
+            <a href="#Projects">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#About">
+              About
+            </a>
+          </li>
+          <li>
+            <button className="pdf-button">
+              Download PDF
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
